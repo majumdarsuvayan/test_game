ec0e58e644780855c8cb73e20f2ea3bbd8e91e29
majumdarsuvayan <81520093+majumdarsuvayan@users.noreply.github.com>
2023-08-16 23:59:53 +0530

    Add files via upload

---
+++ hard-word-list .js
@@ -0,0 +1,207 @@
+if (mode==hard){
+const wordList = [
+    
+        {
+            word: "moderate",
+            hint: "Being within reasonable limits or average"
+        },
+        {
+            word: "intrigue",
+            hint: "Arouse curiosity or interest through unusual or secret actions"
+        },
+        {
+            word: "perplex",
+            hint: "Confuse or puzzle someone"
+        },
+        {
+            word: "endeavor",
+            hint: "An attempt to achieve a goal"
+        },
+        {
+            word: "elaborate",
+            hint: "Involving many intricate details"
+        },
+        {
+            word: "persistent",
+            hint: "Continuing firmly in a course of action despite difficulties"
+        },
+        {
+            word: "evoke",
+            hint: "Bring a feeling, memory, or image to the mind"
+        },
+        {
+            word: "prevalent",
+            hint: "Widely existing or occurring"
+        },
+        {
+            word: "convey",
+            hint: "Communicate or express a thought or feeling"
+        },
+        {
+            word: "meticulous",
+            hint: "Showing great attention to detail and precision"
+        },
+        {
+            word: "dubious",
+            hint: "Fraught with uncertainty or doubt"
+        },
+        {
+            word: "deceptive",
+            hint: "Intending to mislead or trick"
+        },
+        {
+            word: "eloquent",
+            hint: "Fluent and persuasive in speaking or writing"
+        },
+        {
+            word: "skeptical",
+            hint: "Not easily convinced or having doubts"
+        },
+        {
+            word: "quench",
+            hint: "Satisfy a thirst or desire"
+        },
+        {
+            word: "jubilant",
+            hint: "Feeling or expressing great happiness and triumph"
+        },
+        {
+            word: "turbulent",
+            hint: "Characterized by conflict, disorder, or confusion"
+        },
+        {
+            word: "profound",
+            hint: "Having great depth or seriousness"
+        },
+        {
+            word: "novelty",
+            hint: "The quality of being new, original, or unusual"
+        },
+        {
+            word: "equitable",
+            hint: "Fair and impartial"
+        },
+        {
+            word: "dynamic",
+            hint: "Characterized by constant change, activity, or progress"
+        },
+        {
+            word: "resilient",
+            hint: "Able to recover quickly from difficulties or setbacks"
+        },
+        {
+            word: "empower",
+            hint: "Give someone the authority or power to do something"
+        },
+        {
+            word: "meticulous",
+            hint: "Showing great attention to detail and precision"
+        },
+        {
+            word: "deviate",
+            hint: "Depart from an established course or standard"
+        },
+        {
+            word: "integrity",
+            hint: "Adherence to moral and ethical principles"
+        },
+        {
+            word: "prosper",
+            hint: "Flourish or succeed in an endeavor"
+        },
+        {
+            word: "vulnerable",
+            hint: "Open to the possibility of being harmed or attacked"
+        },
+        {
+            word: "precise",
+            hint: "Exact, accurate, and careful about details"
+        },
+        {
+            word: "subtle",
+            hint: "Not obvious, faint, or delicate"
+        },
+        {
+            word: "innovate",
+            hint: "Introduce new methods or ideas"
+        },
+        {
+            word: "ambivalent",
+            hint: "Having mixed or conflicting feelings about something"
+        },
+        {
+            word: "coherent",
+            hint: "Logical and consistent in thought or ideas"
+        },
+        {
+            word: "inclusive",
+            hint: "Including all or encompassing"
+        },
+        {
+            word: "indifferent",
+            hint: "Having no particular interest or concern"
+        },
+        {
+            word: "exemplify",
+            hint: "Serve as a typical example of"
+        },
+        {
+            word: "compatible",
+            hint: "Able to exist or work together without conflict"
+        },
+        {
+            word: "elusive",
+            hint: "Difficult to find, achieve, or catch"
+        },
+        {
+            word: "ambiguous",
+            hint: "Open to more than one interpretation"
+        },
+        {
+            word: "benevolent",
+            hint: "Well-meaning and kindly"
+        },
+        {
+            word: "compelling",
+            hint: "Evoking interest, attention, or admiration"
+        },
+        {
+            word: "provoke",
+            hint: "Stimulate a reaction or feeling, typically deliberately"
+        },
+        {
+            word: "precursor",
+            hint: "Something that comes before and indicates the approach of something else"
+        },
+        {
+            word: "complacent",
+            hint: "Satisfied with one's achievements, often unaware of potential danger"
+        },
+        {
+            word: "receptive",
+            hint: "Willing to consider or accept new suggestions and ideas"
+        },
+        {
+            word: "resolute",
+            hint: "Determined and unwavering"
+        },
+        {
+            word: "vivid",
+            hint: "Bright and distinct in color"
+        },
+        {
+            word: "debilitate",
+            hint: "Make someone weak and infirm"
+        },
+        {
+            word: "prolific",
+            hint: "Producing many works, results, or offspring"
+        },
+        {
+            word: "impartial",
+        }
+    ]
+}
+    
+
+
